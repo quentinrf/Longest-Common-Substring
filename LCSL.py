ef52c1af306e1c0aa4d38a4dcb40fa998f1dcf09
Quentin Roy-Foster <16qrf1@caslab.queensu.ca>
2019-11-07 13:40:50 -0500

    update

--- LCSL.py
+++ LCSL.py
@@ -51,29 +51,37 @@ def fillTable():
 
 #Function to retrace table and find the longest common substring
 def reTrace(LCSL):
+    #set the current cell equal to the bottom right cell
     current = LCSL[len(p)-1][len(q)-1]
+    #set pVal equal to last index of the p string
     pVal = 9
+    #set qVal equal to last index of the q string
     qVal = 7
+    #create an empty list to store the letters
     letters = []
 
+    #while we havent reached the top left corner
     while(current!=0):
-
+        #if the characters match we know this char is in the longest common substring
         if(p[pVal]==q[qVal]):
             current = LCSL[pVal-1][qVal-1]
             letters.append(p[pVal])
             qVal -= 1
             pVal -= 1
+        #otherwise if its equal to the one to the left this is our new cell
         elif(current==LCSL[pVal][qVal-1]):
             current = LCSL[pVal][qVal-1]
             qVal -= 1
+        #otherwise if its equal to the upper one this is our new cell
         elif(current==LCSL[pVal-1][qVal]):
             current = LCSL[pVal-1][qVal]
             pVal -= 1
+        #otherwise if its equal to the upper left one this is our new cell
         elif(current==LCSL[pVal-1][qVal-1]):
             current = LCSL[pVal-1][qVal-1]
             pVal -= 1
             qVal -= 1
-
+    #reverse the list of letters and print them
     letters.reverse()
     print(letters)
 fillTable()
